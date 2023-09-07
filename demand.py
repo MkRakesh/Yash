e31ede4bdda382bb4d255243f1f241cecac12c98
MkRakesh <137186397+MkRakesh@users.noreply.github.com>
2023-09-07 20:52:36 +0530

    Update demand.py

--- demand.py
+++ demand.py
@@ -16,11 +16,12 @@ if input1 != 0 :
     
     forecast_values = fit.predict(start=0, end=input1 - 1)
     plt.figure(figsize=(5,3))
-    plt.plot(forecast_values,linewidth=.8,color='fuchsia')
+    plt.plot(forecast_values,linewidth=.8,color='fuchsia',label='Demand Sale Forecast')
     plt.ylabel('Forecast',fontsize=9)
     plt.xlabel('YYYY-MM',fontsize=9)
     plt.xticks(fontsize=6,rotation=45, ha="right",color='darkmagenta')
     plt.yticks(fontsize=6,color='darkmagenta')
+    plt.legend(loc=best,fontsize=6)
     plt.grid()
     st.write('Demand Forecast')
     
